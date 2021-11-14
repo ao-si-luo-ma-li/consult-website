3c8c4c1d95d84b4c964905bc2cf1dfa007091e87
Yu Chao (余超 分众上海) <yuchao@focusmedia.cn>
2021-11-14 18:17:21 +0800

    某公司信息展示网demo站_2021_11_14_01

---
+++ src/components/common/dt.js
@@ -0,0 +1,92 @@
+/**
+ * 文章详情映射
+ */
+const message_1 = {
+  title: "没有收到$1200, $600纾困金, 怎么办？纾困金需要退还，和报税吗？",
+  content: `
+<div class="entry-content clearfix">
+                    <p>2020疫情打得大家措手不及，很多人的生活和工作都大受影响。为了帮助大家渡过难关，国税局针对部分符合要求的纳税人提供了两轮纾困金帮助（Economic Impact Payment,简称EIP）。</p>
+<p><strong><span style="color: #800000;">第一轮纾困金$1,200</span></strong>在2020年4月已经发下去了。</p>
+<p><strong><span style="color: #800000;">第二轮纾困金$600</span></strong>也在新年第一天开始发放。国税局说大部分符合要求的纳税人都会于1/15/2021之前收到。</p>
+<p><span style="color: #800000;">没有收到怎么办？收到金额不足怎么办？</span></p>
+<p>不用急，我会一一对这些情况进行说明。</p>
+<p>&nbsp;</p>
+<p><span style="font-size: 18px;"><strong>纾困金到底是怎么发放的？</strong></span></p>
+<p><strong>第一轮发放是2020</strong><strong>年4</strong><strong>月</strong>，很多人没有完成2019年的报税。税局根据系统里面有的2018或者2019年的税表数据发放。</p>
+<p><strong>第二轮发放是2021</strong><strong>年1</strong><strong>月初</strong>，绝大部分人已经完成了2019年的报税。税局根据系统里面的2019年的税表数据发放。如果还是没有申报2019税表，仍然会按照2018数据发放。</p>
+<p><noscript><img class="n3VNCb" src="http://www.chencpainc.com/wp-content/uploads/2021/01/d2645333.jpg" alt="快搶美元！美金可望貶至31元暑假想出國快把握機會| ETtoday財經雲| ETtoday新聞雲" data-noaft="1" /></noscript><a class="fluidbox fluidbox__instance-1 fluidbox--initialized fluidbox--closed fluidbox--ready" href="http://www.chencpainc.com/wp-content/uploads/2021/01/d2645333.jpg" data-fluidbox="" data-fluidbox-loader="true"><div class="fluidbox__wrap" style="z-index: 1;"><img class="n3VNCb j-lazy fluidbox__thumb" src="http://www.chencpainc.com/wp-content/themes/Module/themer/assets/images/lazy.png" data-original="http://www.chencpainc.com/wp-content/uploads/2021/01/d2645333.jpg" alt="快搶美元！美金可望貶至31元暑假想出國快把握機會| ETtoday財經雲| ETtoday新聞雲" data-noaft="1" style="opacity: 1;"><div class="fluidbox__ghost" style="width: 48px; height: 32px; top: 0px; left: 0px;"></div><div class="fluidbox__loader" style="z-index: 2;"></div></div></a></p>
+<p>疫情是发生在2020年。很多人受疫情影响，收入大幅减少。<strong>如果根据</strong><strong>2018</strong><strong>，或者2019</strong><strong>数据，是不符合要求领取纾困金的。</strong>但是根据2020年数据，却有资格领取全部或者部分纾困金。这种情况下，您可以在申报2020年税表时，申请Recovery Rebate Credit，您就可以领取到您应得的所有纾困金。</p>
+<p>所以没有收到纾困金的大家不用担心，<span style="color: #ff0000;">只要您2020年税表上申报收入符合要求，您就可以在申报2020年税表时，找税局申请credit。</span></p>
+<p><strong>这个Credit&nbsp; </strong><strong>也适用于其他情况，如</strong></p>
+<p>1: 2020年有新出生的宝宝，可以申请credit；</p>
+<p>2: 2019年是用ITIN, 2020年刚刚拿到社会安全号码，可以申请credit；</p>
+<p>3: 2020年才是来美第六年的留学生，可以申请credit;</p>
+<p>4: 2019年夫妻合报税，一方是ITIN,一方是SSN，第一轮时SSN方没有拿到纾困金，可以</p>
+<p>申请credit;</p>
+<p><strong>一个宗旨，只要您2020</strong><strong>年税表情况符合要求，您就可以申请拿回应得的两轮纾困金补助。</strong></p>
+<p>当然，如果您2020年收入逆风而涨，而不符合领取纾困金的资格。恭喜您。<span style="color: #800000;">只要第一轮，第二轮纾困金已经到了您的账户，这就是您的了。</span>不管您2020年的收入是100万，1000万，还是多少，<strong><span style="color: #800000;">已经到手的纾困金都不用还回去给税局。</span></strong></p>
+<p>&nbsp;</p>
+<p><span style="font-size: 18px;"><strong>纾困金需要交税吗？</strong></span></p>
+<p><span style="color: #ff0000;"><strong>不需要</strong></span><strong><span style="color: #ff0000;">。</span>$1200</strong><strong>和$600</strong><strong>的纾困金，全部免税，不需要申报</strong>。</p>
+<p>需要注意的是，疫情领取的所有失业金是需要报税的哦。</p>
+<p>&nbsp;</p>
+<p>免责申明：本文章提供的信息仅供一般参考。这些信息不构成也不旨在构成法律建议，并且您不应基于本文章提供的任何信息行事或不行事。请就您的具体情况咨询您自己的顾问。本作者/公司不承担任何法律责任。</p>
+                                    </div>
+  `,
+};
+
+const about = {
+  title: "关于我们",
+  content: `
+  <div class="entry-content">
+                        
+<p>　　陈菲会计师事务所(Chen CPA Inc. A Professional Accountancy Corporation) ，是美国加州正式登记的一家以<u>服务</u><u>客户</u>为宗旨的注册会计师事务所（Certified Public Accountant, 简称CPA），拥有丰富的实践经验和高素质的团队，能够敏锐响应客户需求。</p>
+<p>　　本着为客户提供更好的服务体验的宗旨，我们致力于高效率地为客户提供可靠的的，专业的定制化服务。在准确的了解客户情况和需求的前提下，因需求而异为个人和企业提供量身定制的服务，精准打击，为您的财富道路扫清障碍。</p>
+<p>　　在经济全球化大背景的环境下，我们也专注于提供国际化的服务。我们团队的中美教育及生活背景保证我们能够真正精准地了解客户需求，迅速的解答客户疑问，专业而可靠的解决客户的疑难杂症，帮助国际客户克服语言难题。我们将是您国际商路上的左膀右臂，致力为您实现利益的最大化。</p>
+<p>　　我们的主要服务项目包括全美50个州公司注册，会计，各项税务和咨询服务；美国及海外个人税务申报和咨询服务; 外国人个人税号申请等服务。</p>
+  `,
+};
+
+const faq = {
+  title: "FAQ",
+  content: `FAQ 问题集合!!!`,
+};
+
+const contactus = {
+  title: "联系我们",
+  content: `
+  <div class="entry-content">
+                        
+<p><iframe style="border: 0;" tabindex="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3319.5915467097434!2d-117.7677164!3d33.693639!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcdce1948dfd93%3A0x7776bd14a7e1438b!2zOTgwIFJvb3NldmVsdCAjMjAwLCBJcnZpbmUsIENBIDkyNjIw576O5Zu9!5e0!3m2!1szh-CN!2suk!4v1603265641797!5m2!1szh-CN!2suk" width="100%" height="450" frameborder="0" allowfullscreen="allowfullscreen" aria-hidden="false"></iframe></p>
+<p>联系电话：（657）699-3009　　<span style="color: #000000;">洛杉矶时间 周一至周五 上午9点-下午6点</span></p>
+
+
+
+<p>联系邮箱：APRIL@CHEN.CPA / INFO@CHEN.CPA</p>
+
+
+
+<p>公司地址：980 ROOSEVELT， #200， Irvine, CA 92620</p>
+  `,
+};
+
+const service_company = {
+  title: '公司服务',
+  content: `
+  <div class="entry-content">
+                        <p>我们致力于为您的公司提供全方面，国际化的专业服务和真正一站式的服务体验。用最细致缜密，尽职严谨的态度为您解决从公司成立，到公司会计，财务，税务及未来规划的事务。让您不再受繁琐的事务的束缚，有更多的时间和精力去开拓您的生意市场。</p>
+<div class="tabs"><ul class="nav nav-tabs" role="tablist"><li role="presentation" class="active"><a href="#tabs-_scode_201009021205127-0" role="tab" data-toggle="tab">公司成立和注销</a></li><li role="presentation"><a href="#tabs-_scode_201009021205127-1" role="tab" data-toggle="tab">公司会计和财务</a></li><li role="presentation"><a href="#tabs-_scode_201009021205127-2" role="tab" data-toggle="tab">公司税务和规划</a></li><li role="presentation"><a href="#tabs-_scode_201009021205127-3" role="tab" data-toggle="tab">薪水服务</a></li></ul><div class="tab-wrap"><div class="tab-content"><div role="tabpanel" class="tab-pane fade in active" id="tabs-_scode_201009021205127-0"><p>我们可以帮忙注册全美<strong>50个州</strong>的所有公司，常见的有加利福尼亚州，华盛顿州，内华达州，特拉华州，科罗拉多州，怀俄明州，弗罗里达州等等。公司形式Corporation, Partnership, LLC、LP均可。后续也可以为您提供各种营业执照的申请，提供虚拟地址等服务。真正的一站式综合性服务，让您轻松在美国商路起飞。</p><p><br></p><p>陈菲会计师事务所（Chen CPA Inc）向客户提供包含但不限于以下的服务：</p><ol><li>50个州的公司注册和注销服务<br>向您提供注册流程、所需资料和费用、注册完毕后的文件清单，以及维持公司合法运营的基本政府要求。<br>为您的新企业成立给出具有参考性的实体结构建议，以帮助您获得最有利的税收优惠，&nbsp;尽可能小的法律风险，以及最大的操作性和可移植性。企业结构有C类股份有限公司C-Corporation， S类股份有限公司S-Corporation，有限制责任公司LLC，合伙公司和个人独资公司。我们希望帮您选择最适合您需求的业务结构。</li><li>申请联邦税号 EIN。</li><li>申请所有必需的州和地方证件与许可证。</li><li>帮助准备初步的商业计划，以初拟您的营销，管理和财务计划并确定您的启动资金需求。</li><li>帮助您确定必要的启动资金和备用资金来源。</li><li>帮助您评估您的借贷能力，以便您提前知道在需要时可获得的资金。</li><li>协助您创建实体企业，与律师合作，审查递交州务卿办公室的申请资料。</li><li>当您雇用第一批员工时，可以提供工资单和工资税申报的服务。</li><li>规范您公司行为以符合就业法，避免您就收到罚款和员工不满的投诉的可能。</li><li>帮助您确定您的商业保险需求。</li></ol></div><div role="tabpanel" class="tab-pane fade" id="tabs-_scode_201009021205127-1"><p>公司的记账和会计行为是十分琐碎且耗时的工作，而自己记账的时间成本是并不划算的。我们可以代替您完成这项琐碎的工作，妥善为您管理公司帐目，让您有时间和精力专注于公司的发展。</p><p><br></p><p>陈菲会计师事务所（Chen CPA Inc）向客户提供包含但不限于以下的服务：</p><ol><li>QuickBooks软件支持管理<br>帮您选择最适合您的Quickbooks服务，提供您需要的安装，设置及操作等帮助。我们为您提供员工Quickbooks 操作培训。<br><br></li><li>记账服务<br>每个月或每个季度，我们都会为您</li></ol><ul><li>核对您的银行帐户</li><li>为您提供财务报表</li><li>为您对账目上的问题进行解答</li><li>月度，季度，年度的总结</li></ul></div><div role="tabpanel" class="tab-pane fade" id="tabs-_scode_201009021205127-2"><p>面对不断变化，甚至有时难以理解的美国税法。想要达到对企业税务高效系统的管理目标是并不容易的。然而不可否认，合理的规划是成功地合法减少税收义务的关键。我们对复杂税法的熟练掌握以及对新税法变动敏锐的嗅觉保证我们能够帮您依法纳税，根据我们的税务经验为您主动推荐节税的策略，尽可能的最大化您的税后收入。</p><p><br></p><p>陈菲会计师事务所（Chen CPA Inc）向客户提供包含但不限于以下的服务：</p><ol><li>联邦和各州公司所得税表申报<br>我们使用专业软件配合我们丰富的经验，对您的纳税表格进行多次的检查，以确保没有数字计算问题和美国联邦国税局（IRS）会进一步审查的潜在问题。我们会选择电子方式提交您的纳税表格，以保证您能及时提交您的税表，更方便地追踪您的税表申报情况，更快的拿到退税款。</li><li>薪水和薪水税服务<br>我们为您提供专业的员工薪水计算，薪水税的计算和提交，薪水税表的申报，员工福利的设置等服务。</li><li>销售税及其他税务服务<br>我们为您提供销售税及其他地方税务的申报，如城市税，固定资产税等。</li><li>各州年审<br>我们为您提供全美50个州的年审服务。</li><li>税务分析<br>分析您税务情况，提出未来的规划建议，致力于减少您次年的纳税。站在专业的角度向您提供关于收入及赋税可行的调整建议，致力于合理减少您的赋税负担。</li><li>税局沟通代表<br>当您收到税局查账，或者信件时，我们可以成为您的专业代表，代表您与税局沟通，向税局申诉，申请罚款减免等，成为您与税局之间的桥梁。</li></ol></div><div role="tabpanel" class="tab-pane fade" id="tabs-_scode_201009021205127-3"><p>我们为您的公司提供薪水税管理服务。请将计算和安排薪水税这件繁杂的工作交给我们，减轻您的工作负担的同时也减轻在申报薪水税是可能发生的计算错误以及罚款的可能性。</p><p><br></p><p>陈菲会计师事务所（Chen CPA Inc）向客户提供包含但不限于以下的服务：</p><ol><li>通过联邦电子缴纳系统（EFTPS）存入所有联邦税。</li><li>美国联邦国税局税务报告和州税务报告。</li><li>月度，季度，年度薪资税报表，其中包括W-2， W-3和1099表格等。</li><li>帮您计算及按时缴纳联邦和州的薪水税，避免因为迟交和漏交可能导致罚款。</li><li>代发薪水。</li></ol></div><div role="tabpanel" class="tab-pane fade" id="tabs-_scode_201009021205127-4"></div></div></div></div>
+<p>&nbsp;</p>
+<p>除上述服务以外，我们也在这里为您解答其他的疑问。</p>
+<p>请拨打我们的咨询电话（657）699-3009或用几分钟填写下面表格向我们说明您的问题。</p>
+<p>我们很乐意为您提供一个15分钟的免费咨询来了解您的具体需求，并给出我们专业性的建议。</p>
+  `
+}
+
+export default {
+  message_1,
+  about,
+  faq,
+  contactus,
+  service_company
+};
